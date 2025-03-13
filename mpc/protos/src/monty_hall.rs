fac8be0a2603086a1a32cc1d6eff05d13160a266
Franco Nieddu <nieddu@taceo.io>
2025-03-13 15:27:17 +0100

    wip: grpc connection

--- mpc/protos/src/monty_hall.rs
+++ mpc/protos/src/monty_hall.rs
@@ -18,10 +18,10 @@ pub mod mpc_node_service_client {
         dead_code,
         missing_docs,
         clippy::wildcard_imports,
-        clippy::let_unit_value,
+        clippy::let_unit_value
     )]
-    use tonic::codegen::*;
     use tonic::codegen::http::Uri;
+    use tonic::codegen::*;
     #[derive(Debug, Clone)]
     pub struct MpcNodeServiceClient<T> {
         inner: tonic::client::Grpc<T>,
@@ -60,14 +60,13 @@ pub mod mpc_node_service_client {
             F: tonic::service::Interceptor,
             T::ResponseBody: Default,
             T: tonic::codegen::Service<
-                http::Request<tonic::body::BoxBody>,
-                Response = http::Response<
-                    <T as tonic::client::GrpcService<tonic::body::BoxBody>>::ResponseBody,
+                    http::Request<tonic::body::BoxBody>,
+                    Response = http::Response<
+                        <T as tonic::client::GrpcService<tonic::body::BoxBody>>::ResponseBody,
+                    >,
                 >,
-            >,
-            <T as tonic::codegen::Service<
-                http::Request<tonic::body::BoxBody>,
-            >>::Error: Into<StdError> + std::marker::Send + std::marker::Sync,
+            <T as tonic::codegen::Service<http::Request<tonic::body::BoxBody>>>::Error:
+                Into<StdError> + std::marker::Send + std::marker::Sync,
         {
             MpcNodeServiceClient::new(InterceptedService::new(inner, interceptor))
         }
@@ -105,22 +104,12 @@ pub mod mpc_node_service_client {
         pub async fn new_game(
             &mut self,
             request: impl tonic::IntoRequest<super::NewGameRequest>,
-        ) -> std::result::Result<
-            tonic::Response<super::NewGameResponse>,
-            tonic::Status,
-        > {
-            self.inner
-                .ready()
-                .await
-                .map_err(|e| {
-                    tonic::Status::unknown(
-                        format!("Service was not ready: {}", e.into()),
-                    )
-                })?;
+        ) -> std::result::Result<tonic::Response<super::NewGameResponse>, tonic::Status> {
+            self.inner.ready().await.map_err(|e| {
+                tonic::Status::unknown(format!("Service was not ready: {}", e.into()))
+            })?;
             let codec = tonic::codec::ProstCodec::default();
-            let path = http::uri::PathAndQuery::from_static(
-                "/monty_hall.MpcNodeService/NewGame",
-            );
+            let path = http::uri::PathAndQuery::from_static("/monty_hall.MpcNodeService/NewGame");
             let mut req = request.into_request();
             req.extensions_mut()
                 .insert(GrpcMethod::new("monty_hall.MpcNodeService", "NewGame"));
@@ -135,7 +124,7 @@ pub mod mpc_node_service_server {
         dead_code,
         missing_docs,
         clippy::wildcard_imports,
-        clippy::let_unit_value,
+        clippy::let_unit_value
     )]
     use tonic::codegen::*;
     /// Generated trait containing gRPC methods that should be implemented for use with MpcNodeServiceServer.
@@ -167,10 +156,7 @@ pub mod mpc_node_service_server {
                 max_encoding_message_size: None,
             }
         }
-        pub fn with_interceptor<F>(
-            inner: T,
-            interceptor: F,
-        ) -> InterceptedService<Self, F>
+        pub fn with_interceptor<F>(inner: T, interceptor: F) -> InterceptedService<Self, F>
         where
             F: tonic::service::Interceptor,
         {
@@ -225,15 +211,9 @@ pub mod mpc_node_service_server {
                 "/monty_hall.MpcNodeService/NewGame" => {
                     #[allow(non_camel_case_types)]
                     struct NewGameSvc<T: MpcNodeService>(pub Arc<T>);
-                    impl<
-                        T: MpcNodeService,
-                    > tonic::server::UnaryService<super::NewGameRequest>
-                    for NewGameSvc<T> {
+                    impl<T: MpcNodeService> tonic::server::UnaryService<super::NewGameRequest> for NewGameSvc<T> {
                         type Response = super::NewGameResponse;
-                        type Future = BoxFuture<
-                            tonic::Response<Self::Response>,
-                            tonic::Status,
-                        >;
+                        type Future = BoxFuture<tonic::Response<Self::Response>, tonic::Status>;
                         fn call(
                             &mut self,
                             request: tonic::Request<super::NewGameRequest>,
@@ -267,23 +247,19 @@ pub mod mpc_node_service_server {
                     };
                     Box::pin(fut)
                 }
-                _ => {
-                    Box::pin(async move {
-                        let mut response = http::Response::new(empty_body());
-                        let headers = response.headers_mut();
-                        headers
-                            .insert(
-                                tonic::Status::GRPC_STATUS,
-                                (tonic::Code::Unimplemented as i32).into(),
-                            );
-                        headers
-                            .insert(
-                                http::header::CONTENT_TYPE,
-                                tonic::metadata::GRPC_CONTENT_TYPE,
-                            );
-                        Ok(response)
-                    })
-                }
+                _ => Box::pin(async move {
+                    let mut response = http::Response::new(empty_body());
+                    let headers = response.headers_mut();
+                    headers.insert(
+                        tonic::Status::GRPC_STATUS,
+                        (tonic::Code::Unimplemented as i32).into(),
+                    );
+                    headers.insert(
+                        http::header::CONTENT_TYPE,
+                        tonic::metadata::GRPC_CONTENT_TYPE,
+                    );
+                    Ok(response)
+                }),
             }
         }
     }
